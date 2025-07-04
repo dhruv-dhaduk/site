3de88af2821f6d6d0cc71bddf2524d490124c277
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-07-04 16:29:31 +0530

    Remove the unused files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from 'next';
-// import { Geist, Geist_Mono } from 'next/font/google';
 import { IBM_Plex_Mono, Inter } from 'next/font/google';
 import './globals.css';
-// import { Header } from '@/components/Header';
-
-// const geistSans = Geist({
-//     variable: '--font-geist-sans',
-//     subsets: ['latin'],
-// });
-
-// const geistMono = Geist_Mono({
-//     variable: '--font-geist-mono',
-//     subsets: ['latin'],
-// });
 
 const ibmPlexMono = IBM_Plex_Mono({
     subsets: ['latin'],
--- src/components/portfolio/SectionHeading.tsx
+++
@@ -1,4 +0,0 @@
-/** Returns a heading with some styles for page heading */
-export function SectionHeading({ title }: { title: string }) {
-    return <p className="text-3xl font-bold md:text-4xl">{title}</p>;
-}
