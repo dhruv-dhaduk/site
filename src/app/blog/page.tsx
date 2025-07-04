ef07e6a36a20078bf0e2514710df48372b94d83f
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-07-04 19:02:31 +0530

    Create projects, blog, wiki pages with under construction message

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+export default function Blog() {
+    return (
+        <div className="flex h-screen flex-col items-center justify-center">
+            <h1 className="font-inter mb-4 text-4xl font-bold">Blog</h1>
+            <p className="font-plex text-lg text-gray-600">
+                This page is under construction. Stay tuned for updates!
+            </p>
+        </div>
+    );
+}
