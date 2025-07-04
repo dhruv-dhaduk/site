4dbb49be5c2dc1337ab3bd5100a33b085dc70a0d
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-07-04 20:21:10 +0530

    Fix the layout in unimplemented pages

--- src/app/wiki/page.tsx
+++ src/app/wiki/page.tsx
@@ -1,9 +1,12 @@
 export default function Wiki() {
     return (
-        <div className="flex h-screen flex-col items-center justify-center">
+        <div className="flex min-h-dvh flex-col items-center justify-center">
             <h1 className="font-inter mb-4 text-4xl font-bold">Wiki</h1>
             <p className="font-plex text-lg text-gray-600">
-                This page is under construction. Stay tuned for updates!
+                This page is under construction.
+            </p>
+            <p className="font-plex text-lg text-gray-600">
+                Stay tuned for updates!
             </p>
         </div>
     );
