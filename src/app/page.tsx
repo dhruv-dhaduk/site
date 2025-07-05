ef1c9a58f640b951b2699d67c8927332431b1d6e
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-07-05 15:50:39 +0530

    Add gradient text color to name in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,9 @@ export default function Home() {
                 <div className="flex flex-col gap-2">
                     <p className="text-sm text-[#999]">{`Hi I'm`}</p>
                     <h1 className="font-inter flex items-end gap-4 text-5xl font-bold">
-                        <span>Dhruv Dhaduk</span>
+                        <span className="bg-gradient-to-b from-[#ffffffff] via-[#ffffffc8] to-[#ffffff4b] bg-clip-text text-transparent">
+                            Dhruv Dhaduk
+                        </span>
                         <span className="animate-blink mb-1 hidden h-2 w-6 bg-[#E5E5E5] sm:block" />
                     </h1>
                     <p className="flex items-center gap-1.5 text-[#bbbbbb]">
