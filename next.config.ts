c8635910d07da9c0e8e7d7c2de133fbedd9c41f1
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-07-06 19:33:03 +0530

    Add uploadthing URL in next config for image optimization

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
                 hostname: 'raw.githubusercontent.com',
                 pathname: '/dhruv-dhaduk/vault/**',
             },
+            {
+                protocol: 'https',
+                hostname: '8diytvqkng.ufs.sh',
+                pathname: '/**',
+            },
         ],
     },
 };
