16f42ff1f1eec50224d623653043ded364cecf1b
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-07-06 18:29:33 +0530

    Use 'no-store' for caching in projects fetching service

--- src/services/projects.ts
+++ src/services/projects.ts
@@ -7,7 +7,7 @@ const PROJECTS_DATA_URL =
 export async function fetchProjects(): Promise<Array<Project>> {
     console.log('fetchProjects called');
     try {
-        const response = await fetch(PROJECTS_DATA_URL);
+        const response = await fetch(PROJECTS_DATA_URL, { cache: 'no-store' });
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
