6f38e214f13cdc9c49ae1c4d12a25a4ce8dae812
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-07-06 23:19:42 +0530

    Remove commented code

--- src/schema/projects.ts
+++ src/schema/projects.ts
@@ -5,7 +5,6 @@ export const ProjectSchema = z.object({
     id: z.string().trim().nonempty(),
     title: z.string().trim().nonempty(),
     description: z.string().trim().nonempty(),
-    // thumbnail: z.string().trim().nonempty().url(),
     thumbnail: ImageSchema,
     githubLink: z.string().trim().nonempty().url(),
     liveLink: z.string().trim().nonempty().url().nullable(),
