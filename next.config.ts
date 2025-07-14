4a58044b34fc03f45e94fb67a0acb89f2248d1bd
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-07-15 00:03:19 +0530

    Remove github raw url from next image optimization

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,6 @@ const nextConfig: NextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'raw.githubusercontent.com',
-                pathname: '/dhruv-dhaduk/vault/**',
-            },
-            {
-                protocol: 'https',
                 hostname: '8diytvqkng.ufs.sh',
                 pathname: '/**',
             },
