73beb388d06201f5643bf83f87197b8a71740842
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-09-06 16:45:00 +0530

    Use shadcn Select component to show basic communication channel selector in Contact Me form

--- src/components/Contact/ContactMeForm/ContactMeForm.tsx
+++ src/components/Contact/ContactMeForm/ContactMeForm.tsx
@@ -1,28 +1,66 @@
 'use client';
 
 import { Input } from '@/components/ui/input';
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
 
 export function ContactMeForm() {
     return (
-        <div className="mx-auto w-full max-w-lg">
+        <div className="font-inter mx-auto w-full max-w-lg">
             <form
                 onSubmit={(e) => e.preventDefault()}
                 className="flex w-full flex-col gap-4"
             >
-                <div className="flex flex-col">
-                    <label htmlFor="name" className="text-[15px]">
-                        Name
-                    </label>
-                    <Input
-                        id="name"
-                        name="name"
-                        required
-                        aria-required
-                        type="text"
-                        placeholder="Enter Your Name"
-                        className={`border-gray h-10 rounded-lg border pl-3`}
-                    />
+                <div className="flex w-full gap-4">
+                    <div className="flex flex-1 flex-col">
+                        <label htmlFor="name" className="text-[15px]">
+                            Name
+                        </label>
+                        <Input
+                            id="name"
+                            name="name"
+                            required
+                            aria-required
+                            type="text"
+                            placeholder="Enter Your Name"
+                            className={`border-gray h-10 rounded-lg border pl-3`}
+                        />
+                    </div>
+                    <div className="flex flex-1 flex-col">
+                        <label htmlFor="name" className="text-[15px]">
+                            Conversation Channel
+                        </label>
+                        <Select>
+                            <SelectTrigger className="w-full">
+                                <SelectValue placeholder="Select a channel" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectLabel>Channels</SelectLabel>
+                                    <SelectItem value="none">None</SelectItem>
+                                    <SelectItem value="email">Email</SelectItem>
+                                    <SelectItem value="telegram">
+                                        Telegram
+                                    </SelectItem>
+                                    <SelectItem value="teams">
+                                        MS Teams
+                                    </SelectItem>
+                                    <SelectItem value="slack">Slack</SelectItem>
+                                    <SelectItem value="discord">
+                                        Discord
+                                    </SelectItem>
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    </div>
                 </div>
                 <div className="flex flex-col">
                     <label htmlFor="email" className="text-[15px]">
