f2d2bdfe6e2ef8620664115ec0b7ce56a6c98eab
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-09-13 14:23:06 +0530

    Create seperate services for telegram and resend in contact feature

--- src/features/contact/server/submit.ts
+++ src/features/contact/server/submit.ts
@@ -1,30 +1,21 @@
 'use server';
 
-import { resend } from '@/lib/resend';
+import { logMessageToTelegram } from '$/contact/services/telegram';
+import { startEmailConversation } from '$/contact/services/resend';
 import {
     ContactChannel,
     ContactSchema,
     type ContactFormData,
 } from '$/contact/schemas/contact.schema';
-import { formatFormDataForTelegram } from '$/contact/utils';
-
-import { ContactEmailTemplate } from '$/contact/components/ContactEmailTemplate';
-
-const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
-const RESEND_EMAIL_ADDRESS = process.env.RESEND_EMAIL_ADDRESS;
-const PERSONAL_EMAIL_ADDRESS = process.env.PERSONAL_EMAIL_ADDRESS;
 
 /**
  * Processes the contact form data by validating it and sending it to a Telegram chat.
  * @param data - The contact form data to be processed.
  * @throws Will throw an error if the environment variables are not set, if the data is invalid, or if the Telegram API request fails.
  */
-export async function processContactForm(data: ContactFormData) {
-    if (!TELEGRAM_BOT_TOKEN || !TELEGRAM_CHAT_ID) {
-        throw new Error('Internal Server Error.');
-    }
-
+export async function processContactForm(
+    data: ContactFormData
+): Promise<{ message: string }> {
     const parsed = ContactSchema.safeParse(data);
     if (!parsed.success) {
         throw new Error('The data you submitted is invalid.');
@@ -32,54 +23,21 @@ export async function processContactForm(data: ContactFormData) {
 
     const parsedData = parsed.data;
 
-    const text = formatFormDataForTelegram(parsedData);
-    const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+    await logMessageToTelegram(parsedData);
 
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            chat_id: TELEGRAM_CHAT_ID,
-            text,
-            parse_mode: 'MarkdownV2',
-        }),
-    });
-
-    const responseJson = await response.json();
-
-    if (!response.ok || !responseJson.ok) {
-        console.log(responseJson);
-        throw new Error('An unexpected error occured.');
+    switch (parsedData.channel) {
+        case ContactChannel.EMAIL:
+            await startEmailConversation(
+                parsedData.name,
+                parsedData.channelAccount,
+                parsedData.message
+            );
+            return {
+                message: 'Done. Check your email for confirmation.',
+            };
+        default:
+            return {
+                message: 'Message sent successfully.',
+            };
     }
-
-    if (parsedData.channel === ContactChannel.EMAIL) {
-        if (!RESEND_EMAIL_ADDRESS || !PERSONAL_EMAIL_ADDRESS) {
-            throw new Error('Internal Server Error.');
-        }
-
-        const response = await resend.emails.send({
-            from: `${parsedData.name} <${RESEND_EMAIL_ADDRESS}>`,
-            to: [PERSONAL_EMAIL_ADDRESS],
-            cc: parsedData.channelAccount,
-            replyTo: parsedData.channelAccount,
-            subject: `Contact Dhruv Dhaduk | ${parsedData.name} <${parsedData.channelAccount}>`,
-            react: ContactEmailTemplate({
-                name: parsedData.name,
-                message: parsedData.message,
-                email: parsedData.channelAccount,
-            }),
-        });
-
-        if (response.error) {
-            throw new Error(response.error.message);
-        }
-
-        return {
-            message: 'Done. Check your email for confirmation.',
-        };
-    }
-
-    return {
-        message: 'Message sent successfully.',
-    };
 }
---
+++ src/features/contact/services/telegram.ts
@@ -0,0 +1,35 @@
+import 'server-only';
+
+import { env } from '@/env';
+
+import { type ContactFormData } from '$/contact/schemas/contact.schema';
+import { formatFormDataForTelegram } from '$/contact/utils';
+
+/**
+ * Logs a message to predefined Telegram chat using the Telegram Bot API.
+ * @param data - The contact form data to be sent to Telegram.
+ * @throws Will throw an error if the Telegram API request fails.
+ */
+export async function logMessageToTelegram(
+    data: ContactFormData
+): Promise<void> {
+    const url = `https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage`;
+    const text = formatFormDataForTelegram(data);
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            chat_id: env.TELEGRAM_CHAT_ID,
+            text,
+            parse_mode: 'MarkdownV2',
+        }),
+    });
+
+    const responseJson = await response.json();
+
+    if (!response.ok || !responseJson.ok) {
+        console.log(responseJson);
+        throw new Error('An unexpected error occured.');
+    }
+}
