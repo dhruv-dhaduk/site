973b3d68f7828057cfe2aebca544a39bc399fbe5
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-10-21 16:59:16 +0530

    Enable logs for fetch requests

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     /* config options here */
     typedRoutes: true,
+    logging: {
+        fetches: {
+            fullUrl: true,
+        },
+    },
     images: {
         remotePatterns: [
             {
