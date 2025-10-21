28b9b35d326075fcabc88e9884e1754b70b79a59
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-10-21 11:46:20 +0530

    Restructure the routes for blog and blog post pages

---
+++ src/app/blog/[slug]/layout.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Blog Post | Dhruv Dhaduk',
+    description:
+        'Explore the blog of Dhruv Dhaduk, where I share my thoughts, experiences, and insights on various topics.',
+};
+
+export default function BlogLayout({
+    children,
+}: Readonly<{ children: React.ReactNode }>) {
+    return <div className="mx-auto w-full max-w-[80rem]">{children}</div>;
+}
