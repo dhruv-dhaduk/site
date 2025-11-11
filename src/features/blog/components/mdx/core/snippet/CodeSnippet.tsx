622814a47011b5d1c16eda1535292ae2c6358449
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-11-11 23:28:01 +0530

    Add syntax highlighting to snippets using shiki only

--- src/features/blog/components/mdx/core/snippet/CodeSnippet.tsx
+++ src/features/blog/components/mdx/core/snippet/CodeSnippet.tsx
@@ -1,7 +1,9 @@
+import { codeToHtml } from 'shiki';
+
 import { CopyButton } from './CopyButton';
 import { getLanguageLabel, parseLanguageFromClassName } from './utils';
 
-export function CodeSnippet(props: React.HTMLAttributes<HTMLElement>) {
+export async function CodeSnippet(props: React.HTMLAttributes<HTMLElement>) {
     const { className, children } = props;
 
     if (typeof children !== 'string') {
@@ -16,16 +18,23 @@ export function CodeSnippet(props: React.HTMLAttributes<HTMLElement>) {
 
     const languageLabel = getLanguageLabel(language);
 
+    const highlightedCode = await codeToHtml(children, {
+        lang: language,
+        theme: 'github-dark',
+        structure: 'inline',
+    });
+
     return (
-        <div className="border-site-border-2 my-4 rounded-lg border bg-black/90 text-white">
+        <div className="border-site-border-2 my-4 overflow-hidden rounded-lg border bg-black/90 text-white">
             <div className="border-site-border-2 flex items-center justify-between border-b p-4 py-2">
                 <p className="font-bold">{languageLabel}</p>
                 <CopyButton codeToCopy={children} />
             </div>
-            <div className="snippet-scrollbar overflow-x-auto p-4 text-sm">
-                <code className="text-site-fg-1 w-fit bg-black/90 px-1.5 py-0.5 font-mono text-sm">
-                    {children}
-                </code>
+            <div className="snippet-scrollbar overflow-x-auto bg-[#060606] p-4 text-sm">
+                <code
+                    className="text-site-fg-1 w-fit py-0.5 font-mono text-sm"
+                    dangerouslySetInnerHTML={{ __html: highlightedCode }}
+                ></code>
             </div>
         </div>
     );
